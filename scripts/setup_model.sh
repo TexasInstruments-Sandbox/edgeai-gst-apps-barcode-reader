ce30229965acb460c1f2829fa6497011b314f9e8
reesegrimsley <127260155+reesegrimsley@users.noreply.github.com>
2023-10-06 15:58:33 -0500

    Update setup_model.sh
    Update model URL

--- scripts/setup_model.sh
+++ scripts/setup_model.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+#URL_8_6="https://software-dl.ti.com/jacinto7/esd/edgeai-marketplace/barcode-reader/barcode-modelartifacts.tar.gz"
+URL="https://software-dl.ti.com/jacinto7/esd/edgeai-marketplace/barcode-reader/09_00_00/barcode-modelartifacts.tar.gz"
+
 if [  -f barcode-modelartifacts.tar.gz ] ; then 
     echo "model already downloaded"
 else 
-    wget https://software-dl.ti.com/jacinto7/esd/edgeai-marketplace/barcode-reader/barcode-modelartifacts.tar.gz
+    wget $URL
     if [ "$?" -ne "0" ]; then
-        wget https://software-dl.ti.com/jacinto7/esd/edgeai-marketplace/barcode-reader/barcode-modelartifacts.tar.gz --proxy off
+        wget $URL --proxy off
 
         if [ "$?" -ne "0" ]; then
             echo "Failed to download model; check proxy settings/environment variables. Alternatively, download the model on a PC and transfer to this directory"
